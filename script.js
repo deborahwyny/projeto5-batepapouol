616d443afad290003e176359a56a2fb78f4518d2
Deborah Wyny <deborahwyny25@gmail.com>
2023-04-15 15:18:14 -0300

    entrar na sala e buscar mensagem

--- script.js
+++ script.js
@@ -3,41 +3,142 @@ axios.defaults.headers.common['Authorization'] = 'ufs6rqUwCkDYvURpjxAObPdf';
 // pegar conteudo  mensagens 
 const conteudoMensagens = []
 
-const promessa = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
+const mensagem = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
+mensagem.then(receberMensagens)
+mensagem.catch(erroMensagens)
+
+
+
+function receberMensagens (mensagensRecebidas) {
+    const recebMensagens = document.querySelector('.boxMensagens');
+    recebMensagens.innerHTML = '';
+    console.log(mensagensRecebidas.data);
+
+    let mensagens = mensagensRecebidas.data
+
+    for (let i = 0; i < mensagens.length; i++){
+        let msg = mensagens[i];
+
+        if (msg.type === "status") {
+
+        let mens = document.createElement('li')
+        mens.className = "mensagem status"
+        mens.innerHTML = `<span>(${msg.time}) </span><strong>${msg.from} </strong> ${msg.text}`
+        recebMensagens.appendChild(mens)
+
+        }
+
+        else {
+
+        let mens = document.createElement('li')
+        mens.className = "mensagem"
+        mens.innerHTML = `<span>(${msg.time}) </span><strong>${msg.from}</strong> para <strong>${msg.to}</strong>: ${msg.text}`
+        recebMensagens.appendChild(mens)
+        }
+
+    }
+    
+
+    // for (let i = 0; i < conteudoMensagens.length; i++)
+    // let boxMensagem = boxMensagens [i]
+
+    // recebMensagens += 
+}
+
+function erroMensagens (mensagensErr) {
+    alert('Erro ao receber mensagens!')
+}
+
+
+function statusUsuario () {
+    const status = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', usuario);
+    console.log(status)
+}
+
+
+
+
+
 
 
 // enviar novo de usuario
 let nomeUsuario = prompt('Qual o seu nome?');
-
-const usuario = {
+let usuario = {
     name: nomeUsuario
 }
 
-const promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', usuario)
-promessa.then(enviarUsuario);
-promessa.catch(enviarUsuarioErro)
 
+postarUsuario(usuario);
+
+setTimeout(statusUsuario(), 5000)
+
+
+
+
+function postarUsuario(user) {
+    const promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', user)
+    promessa.then(enviarUsuario);
+    promessa.catch(enviarUsuarioErro)
+}
 
 function enviarUsuario (usuarioRecebido) {
-console.log(usuarioRecebido)
+    alert('Vamos tc?')
+
 }
 
+
 function enviarUsuarioErro (usuarioErro){
-console.log(usuarioErro)
 if(usuarioErro.response.status === 400) {
     alert('Nome de usuário já existente')
+    nomeUsuario = prompt('Novamente o seu nome?');
+
+         usuario = {
+        name: nomeUsuario
+    }
+    postarUsuario(usuario);
 }
 }
+
 
 
 
 // enviar mensagem 
 
 
-
-
 function enviarMensagem () {
     let conteudoMensagem = document.querySelector('#texto').value
-    console.log(conteudoMensagem)
+
+let hoje = new Date();
+let hora = hoje.getHours().toString().padStart(2,'0');
+let minutos = hoje.getMinutes().toString().padStart(2, '0');
+let segundos = hoje.getSeconds().toString().padStart(2, '0');
+let horario = `${hora}:${minutos}:${segundos}`
+
+
+
+let mensagem = {
+    from: nomeUsuario,
+    to: "Todos",
+    text: conteudoMensagem,
+    type: "message",
+    time: horario 
+}
+console.log(mensagem)
+
 
 }
+
+function enviandoMens () {
+let mens = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', mensagem)
+mens.then(mensagemEnviada)
+mens.catch(erroAoEnviar)
+}
+
+
+function mensagemEnviada (deuCerto) {
+console.log(deuCerto)
+}
+
+function erroAoEnviar (errou) {[
+    alert('Erro ao enviar mensagem!')
+]}
